3c285416143b8dd1b7b9e9b092a8391496d674eb
GosuMarti <martiveselinov@gmail.com>
2023-12-24 18:21:39 +0200

    add some validators

--- src/main/java/com/exam/sirma/PairOfEmployees/FileHandler/CsvReader.java
+++ src/main/java/com/exam/sirma/PairOfEmployees/FileHandler/CsvReader.java
@@ -1,14 +1,7 @@
 package com.exam.sirma.PairOfEmployees.FileHandler;
 
 import com.exam.sirma.PairOfEmployees.Model.Employee;
-import com.exam.sirma.PairOfEmployees.Repository.EmployeeRepository;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.stereotype.Component;
-
 import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -16,23 +9,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CsvReader {
-    public static List<Employee> read(Resource resource){
+    public static List<Employee> read(Resource resource) {
         List<Employee> employees = new ArrayList<>();
         try (InputStream inputStream = resource.getInputStream();
              InputStreamReader reader = new InputStreamReader(inputStream);
-             BufferedReader br =  new BufferedReader((reader))){
+             BufferedReader br = new BufferedReader((reader))) {
             String line;
-            while ((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
                 try {
                     long id = Long.parseLong(values[0]);
                     int projectId = Integer.parseInt(values[1]);
                     LocalDate dateFrom = LocalDate.parse(values[2], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-                    LocalDate dateTo = LocalDate.parse(values[3], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-
-                    if (values[1].isEmpty() || values[2].isEmpty() || values[3].isEmpty()) {
-                        System.err.println("Invalid data format.");
-                        continue;
+                    LocalDate dateTo;
+                    if (values.length == 2) {
+                        dateTo = LocalDate.now();
+                    } else {
+                        try {
+                            dateTo = LocalDate.parse(values[3], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                        } catch (Exception exception) {
+                            dateTo = LocalDate.now();
+                        }
                     }
 
                     employees.add(new Employee(id, projectId, dateFrom, dateTo));
