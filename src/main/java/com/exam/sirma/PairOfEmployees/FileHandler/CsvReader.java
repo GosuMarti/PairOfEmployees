2aa12b95988147770ea95ba7a6d89bfddc1d7f40
GosuMarti <martiveselinov@gmail.com>
2023-12-24 23:54:59 +0200

    add parseDate method for different date formats

--- src/main/java/com/exam/sirma/PairOfEmployees/FileHandler/CsvReader.java
+++ src/main/java/com/exam/sirma/PairOfEmployees/FileHandler/CsvReader.java
@@ -5,8 +5,11 @@ import org.springframework.core.io.Resource;
 import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.exam.sirma.PairOfEmployees.Service.DateFormatParser.parseDate;
 
 public class CsvReader {
     public static List<Employee> read(Resource resource) {
@@ -20,15 +23,13 @@ public class CsvReader {
                 try {
                     long id = Long.parseLong(values[0]);
                     int projectId = Integer.parseInt(values[1]);
-                    LocalDate dateFrom = LocalDate.parse(values[2], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-                    LocalDate dateTo;
-                    if (values.length == 2) {
-                        dateTo = LocalDate.now();
-                    } else {
+                    LocalDate dateFrom = parseDate(values[2]);
+                    LocalDate dateTo = LocalDate.now();
+                    if (values.length > 3) {
                         try {
-                            dateTo = LocalDate.parse(values[3], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-                        } catch (Exception exception) {
-                            dateTo = LocalDate.now();
+                            dateTo = parseDate(values[3]);
+                        } catch (DateTimeParseException exception) {
+                            System.err.println("NULL format date isn't an exception, but check csv file still.");
                         }
                     }
 
