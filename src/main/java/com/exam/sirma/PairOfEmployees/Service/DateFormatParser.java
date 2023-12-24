1b4cd8aa0985170969f5a6943774586e27f2bd30
GosuMarti <martiveselinov@gmail.com>
2023-12-24 23:53:31 +0200

    create dateFormatParser for different dateformats

--- src/main/java/com/exam/sirma/PairOfEmployees/Service/DateFormatParser.java
+++ src/main/java/com/exam/sirma/PairOfEmployees/Service/DateFormatParser.java
@@ -1,2 +1,24 @@
-package com.exam.sirma.PairOfEmployees.Service;public class DateFormatParser {
+package com.exam.sirma.PairOfEmployees.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.List;
+
+public class DateFormatParser {
+    public static LocalDate parseDate(String dateString) {
+        List<String> dateFormats = Arrays.asList("dd/MM/yyyy", "yyyy-MM-dd", "MM/dd/yyyy", "yyyy/MM/dd", "yyyy/dd/MM");
+
+        for (String format : dateFormats) {
+            try {
+                return LocalDate.parse(dateString, DateTimeFormatter.ofPattern(format));
+            } catch (DateTimeParseException e) {
+                // Try the next format
+            }
+        }
+
+        System.err.println("Unable to parse date: " + dateString + ". Using default value.");
+        return LocalDate.now();
+    }
 }
